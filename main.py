ac014a9be904115b33aaf26ac23ec36e59450cee
Pushkal-Gupta <pushkalgupta2005@gmail.com>
2026-02-23 11:26:55 +0530

    Integrated llama3.1 model with streamlit for AI prompts

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import streamlit as st
 #Using Selenium Scraper inside Streamlit
 from scrape import scrape_website,extract_body_content,clean_body_content,split_dom_content
 
+#Using Ollama
+from parse import parse_with_ollama
+
 st.title("AI Web Scraper")
 
 url = st.text_input("Enter a Website URL : ")
@@ -19,3 +22,14 @@ if st.button("Scrape Site"):
 
   with st.expander("View DOM Content"):
     st.text_area("DOM_Content",cleaned_content,height = 300)
+
+if "dom_content" in st.session_state:
+  parse_description = st.text_area("What do you want to parse?")
+
+  if st.button("Parse Content"):
+    if parse_description:
+      st.write("Parsing the content")
+
+      dom_chunks = split_dom_content(st.session_state.dom_content)
+      result = parse_with_ollama(dom_chunks,parse_description)
+      st.write(result)
